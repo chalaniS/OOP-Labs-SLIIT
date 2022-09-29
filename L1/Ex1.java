471efc44f3b6ff1feb75eaf8c30930994b7da23b
Chalani Saumya Rajapaksha <chalasaumya@gmail.com>
2022-09-29 10:27:52 +0530

    first commit

---
+++ L1/Ex1.java
@@ -0,0 +1,52 @@
+package L1;
+
+public class Ex1 {
+
+    public static void main(String args[]) {
+
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("*\t");
+            }
+
+            System.out.println("\n");
+
+        }
+
+        System.out.println("\n");
+
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = 5; j >= i; j--) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*  ");
+            }
+
+            System.out.println("\n");
+
+        }
+
+        System.out.println("\n\n");
+
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(" * ");
+            }
+            for (int j = 5; j >= i; j--) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*  ");
+            }
+
+            System.out.println("\n");
+
+        }
+
+    }
+
+}
